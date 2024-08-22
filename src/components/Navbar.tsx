c0f770a17e89d52acc9bb10fcd63eb77d4d96aad
bonk-dev <github.p0j6t@passinbox.com>
2024-08-22 15:23:41 +0200

    Add a drawer menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,9 @@ import Logo from "./Logo.tsx";
 import EditPencilIcon from "../assets/icons/EditPencilIcon.tsx";
 import GitHubIcon from "../assets/icons/GitHubIcon.tsx";
 import AtSymbolIcon from "../assets/icons/AtSymbolIcon.tsx";
-import React from "react";
+import React, {useState} from "react";
+import BarsIcon from "../assets/icons/BarsIcon.tsx";
+import CrossIcon from "../assets/icons/CrossIcon.tsx";
 
 const NavbarLinkBackground = () => {
     return (
@@ -16,10 +18,11 @@ interface NavbarLinkProps {
     children: React.ReactNode,
     icon: React.ReactNode,
     smallPadding: boolean,
-    to: string
+    to: string,
+    onClick?: (to: string) => void
 }
 
-const NavbarLink = ({ children, icon, smallPadding, to }: NavbarLinkProps) => {
+const NavbarLink = ({ children, icon, smallPadding, to, onClick }: NavbarLinkProps) => {
     const paddingClass = smallPadding
         ? 'px-4 py-5'
         : 'px-12 py-5';
@@ -28,8 +31,14 @@ const NavbarLink = ({ children, icon, smallPadding, to }: NavbarLinkProps) => {
     const hoverClass = 'hover:text-white';
     const className = `${paddingClass} ${generalClass} ${hoverClass}`;
 
+    const handleOnClick = () => {
+        if (onClick == null) return;
+
+        onClick(to);
+    };
+
     return (
-        <a href={to} className={className}>
+        <a href={to} className={className} onClick={handleOnClick}>
             <NavbarLinkBackground/>
             <div aria-hidden='true' className='inline [&>svg]:inline mr-3 [&>*]:w-7 [&>*]:h-7'>
                 {icon}
@@ -40,16 +49,18 @@ const NavbarLink = ({ children, icon, smallPadding, to }: NavbarLinkProps) => {
 };
 
 interface NavbarLinksProps {
-    smallPadding: boolean
+    smallPadding: boolean,
+    onClick?: (to: string) => void
 }
 
-export const NavbarLinks = ({ smallPadding }: NavbarLinksProps) => {
+export const NavbarLinks = ({ smallPadding, onClick }: NavbarLinksProps) => {
     return (
         <>
             <NavbarLink
                 icon={<EditPencilIcon/>}
                 smallPadding={smallPadding}
                 to={'#projects'}
+                onClick={onClick}
             >
                 Projects
             </NavbarLink>
@@ -57,6 +68,7 @@ export const NavbarLinks = ({ smallPadding }: NavbarLinksProps) => {
                 icon={<GitHubIcon/>}
                 smallPadding={smallPadding}
                 to={'#'}
+                onClick={onClick}
             >
                 GitHub
             </NavbarLink>
@@ -64,12 +76,37 @@ export const NavbarLinks = ({ smallPadding }: NavbarLinksProps) => {
                 icon={<AtSymbolIcon/>}
                 smallPadding={smallPadding}
                 to={'#contact'}
+                onClick={onClick}
             >
                 Contact
             </NavbarLink>
         </>
     );
+};
 
+interface DrawerMenuProps {
+    show: boolean,
+    onDismiss: () => void
+}
+
+const DrawerMenu = ({ show, onDismiss }: DrawerMenuProps) => {
+    const showClassName = show
+        ? 'translate-x-0'
+        : '-translate-x-full';
+
+    return (
+        <div className={`fixed z-20 w-screen top-0 bottom-0 left-0 ${showClassName} bg-violet-600 transition-transform
+                         px-3`}
+        >
+            <button className='px-3 py-4 cursor-pointer w-fit' onClick={onDismiss}>
+                <CrossIcon className={'w-9 h-9'}/>
+            </button>
+
+            <div className='[&>*]:w-full'>
+                <NavbarLinks smallPadding={true} onClick={onDismiss}/>
+            </div>
+        </div>
+    );
 };
 
 interface NavbarProps {
@@ -81,13 +118,29 @@ export const Navbar = ({ show }: NavbarProps) => {
         ? ''
         : '-translate-y-full';
 
+    const [showDrawer, setShowDrawer] = useState(false);
+    const onDrawerDismiss = () => {
+        setShowDrawer(false);
+    };
+    const onDrawerShow = () => {
+        setShowDrawer(true);
+    };
+
     return (
         <nav className={`fixed top-0 left-0 right-0 z-10 lg:px-16 xl:px-52 2xl:px-80 bg-stone-100 
                          dark:bg-stone-950 py-4 flex flex-row justify-center lg:justify-between items-center
                          font-mono tracking-[.61em] uppercase transition-transform ${visibilityClassNamePostfix}`}>
+            <DrawerMenu show={showDrawer} onDismiss={onDrawerDismiss}/>
+
+            <div className='lg:hidden absolute top-1/2 -translate-y-1/2 left-0 pl-7 cursor-pointer'
+                 onClick={onDrawerShow}
+            >
+                <BarsIcon className={'h-7 w-7'}/>
+            </div>
+
             <a className='flex flex-row items-center gap-3 self-center' href='/'>
-                <Logo includeMd5={false} width={'55'} height={'48'}/>
-                <p className='font-mono font-medium text-3xl mt-2 tracking-normal text-black dark:text-white'>dpago.dev</p>
+                <Logo includeMd5={false} className={'w-10 h-10 lg:w-12 lg:h-12'}/>
+                <p className='font-mono font-medium text-xl md:text-3xl mt-2 tracking-normal text-black dark:text-white'>dpago.dev</p>
             </a>
 
             <div className='hidden lg:block'>
